c8f2941d0f1aaa9407ce28abf63e5b2d1b799ece
Charles Archibong <info@charlesarchibong.copm>
2022-01-18 07:18:25 +0100

    Completed the contract and tested using https://remix.ethereum.org

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,9 +3,39 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Lottery {
-    address public manager;
-
     constructor() {
         manager = msg.sender;
+    }
+
+    address public manager;
+
+    address[] public players;
+
+    address[] public winners;
+
+    function enter() public payable {
+        require(
+            msg.value > .01 ether,
+            "You must deposit at least 0.01eth ether"
+        );
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
+    }
+
+    function pickWinner() public payable {
+        uint256 winner = random() % players.length;
+        address winnerAddress = players[winner];
+        payable(winnerAddress).transfer((address(this).balance * 60) / 100);
+        payable(manager).transfer((address(this).balance * 40) / 100);
+        winners.push(winnerAddress);
+        players = new address[](0);
     }
 }
